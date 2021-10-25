29ba3d78db979f9f2efffbbed32b48e94cd4f5a8
doufudoufuyang <kaokaoyang030@gmail.com>
2021-10-24 22:28:17 -0700

    staybooking backend

---
+++ src/main/java/com/laioffer/staybooking/repository/LocationRepository.java
@@ -0,0 +1,12 @@
+package com.laioffer.staybooking.repository;
+
+import com.laioffer.staybooking.model.Location;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LocationRepository extends ElasticsearchRepository<Location, Long>, CustomLocationRepository {
+
+}
---
+++ src/main/java/com/laioffer/staybooking/service/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.laioffer.staybooking.service;
+
+import com.laioffer.staybooking.repository.AuthorityRepository;
+import com.laioffer.staybooking.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import com.laioffer.staybooking.exception.UserNotExistException;
+import com.laioffer.staybooking.model.Authority;
+import com.laioffer.staybooking.model.Token;
+import com.laioffer.staybooking.model.User;
+import com.laioffer.staybooking.model.UserRole;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private AuthenticationManager authenticationManager;
+    private AuthorityRepository authorityRepository;
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthenticationService(AuthenticationManager authenticationManager, AuthorityRepository authorityRepository, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.authorityRepository = authorityRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Token authenticate(User user, UserRole role) throws UserNotExistException {
+        //1. Check user credential
+        //2. Create token if the user is valid
+        //3. Generate token for the user
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        } catch (AuthenticationException exception) {
+            throw new UserNotExistException("User Doesn't Exist");
+        }
+
+        Authority authority = authorityRepository.findById(user.getUsername()).orElse(null);
+        if (authority == null || !authority.getAuthority().equals(role.name())) {
+            throw new UserNotExistException("User Doesn't Exist");
+        }
+
+        return new Token(jwtUtil.generateToken(user.getUsername()));
+    }
+
+}
+
